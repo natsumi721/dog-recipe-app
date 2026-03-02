212d7da6bbb05748fc56a0bd957ab03a1d6ad6a0
Natsumi Hasegawa <chura721@icloud.com>
2026-03-02 18:22:28 +0900

    レシピをセレクトに合わせて３つ出力

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -1,7 +1,16 @@
 class RecipesController < ApplicationController
     def index
-        @recipes = Recipe.all
-    end
+        if params[:dog_id]
+            dog = Dog.find(params[:dog_id])
 
-    
+            @recipes = Recipe.where(
+                age_stage: dog.age_stage,
+                body_type: dog.body_type,
+                activity_level: dog.activity_level
+            ).order("RANDOM()").limit(3)
+
+        else
+            @recipes = Recipe.all
+        end
+    end 
 end
