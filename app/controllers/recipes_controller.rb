58964f3465153696a84656efe6dde59266f8e9eb
Natsumi Hasegawa <chura721@icloud.com>
2026-03-04 12:47:27 +0900

    アレルギー登録したレシピを提供しないように修正

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -1,18 +1,12 @@
 class RecipesController < ApplicationController
     def index
-        if params[:dog_id]
-            dog = Dog.find(params[:dog_id])
-
-            @recipes = Recipe.where(
-                age_stage: dog.age_stage,
-                body_type: dog.body_type,
-                activity_level: dog.activity_level
-            ).order("RANDOM()").limit(3)
-
-        else
-            @recipes = Recipe.all
-        end
-    end 
+       if params[:dog_id]
+          dog = Dog.find(params[:dog_id])
+          @recipes = dog.recommended_recipes
+       else
+        @recipes =Recipe.limit(3)
+       end
+    end
 
     def show
         @recipe = Recipe.find(params[:id])
