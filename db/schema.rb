89b2dfacba5e1bf7f7a1b06557ada816530ca48c
Natsumi Hasegawa <chura721@icloud.com>
2026-03-06 18:27:33 +0900

    nameをfirst_name:last_nameに変更

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.2].define(version: 2026_03_06_043618) do
+ActiveRecord::Schema[7.2].define(version: 2026_03_06_092644) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -48,6 +48,8 @@ ActiveRecord::Schema[7.2].define(version: 2026_03_06_043618) do
     t.datetime "updated_at", null: false
     t.string "crypted_password"
     t.string "salt"
+    t.string "first_name"
+    t.string "last_name"
     t.index ["email"], name: "index_users_on_email", unique: true
   end
 
