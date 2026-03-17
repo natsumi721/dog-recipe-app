900b9aefee98bafc8576d914db03c69a8222edcb
Natsumi Hasegawa <chura721@icloud.com>
2026-03-17 19:27:31 +0900

    ブックマークを各犬ごとにみれるように編集

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -27,16 +27,49 @@ class RecipesController < ApplicationController
     def show
         @recipe = Recipe.find(params[:id])
       # ログインしている場合のみブックマーク情報を取得
-      if logged_in?
-        @bookmark = current_user.bookmarks.find_by(recipe: @recipe)
-      end
+  if logged_in?
+    # 🐶 dog_id が渡されている場合
+    if params[:dog_id].present?
+      @dog = current_user.dogs.find(params[:dog_id])
+      @bookmark = current_user.bookmarks.find_by(recipe: @recipe, dog: @dog)
+    else
+      # 🐶 1頭だけの場合は自動で設定
+      @dog = current_user.dogs.first
+      @bookmark = current_user.bookmarks.find_by(recipe: @recipe, dog: @dog)
+    end
+  end
+      @return_to = params[:return_to]
     end
 
     def bookmarks
-      @bookmark_recipes = current_user.bookmark_recipes.includes(:user).order(created_at: :desc)
+    dogs = current_user.dogs
+
+    # 🐶 1頭だけなら自動でその子のブックマークへ
+    if params[:dog_id].blank? && dogs.count == 1
+      return redirect_to bookmarks_recipes_path(dog_id: dogs.first.id)
     end
 
-    def select_dog
-      @dogs = current_user.dogs
+    # 🐶 dog選択後 or 1頭のみ
+    if params[:dog_id].present?
+      @dog = dogs.find(params[:dog_id])
+      
+      @bookmark_recipes = current_user.bookmarks
+                                   .where(dog_id: @dog.id)
+                                   .includes(:recipe)
+                                   .map(&:recipe)
+      
+      # 🐶 bookmarks_list.html.erb を表示
+      render :bookmarks_list
+    else
+      # 🐶 複数頭のときだけ選択画面
+      @dogs = dogs
+      
+      # 🐶 bookmarks_select.html.erb を表示
+      render :bookmarks_select
     end
+  end
+
+  def select_dog
+    @dogs = current_user.dogs
+  end
 end
--- app/models/bookmark.rb
+++ app/models/bookmark.rb
@@ -1,6 +1,7 @@
 class Bookmark < ApplicationRecord
     belongs_to :user
     belongs_to :recipe
+    belongs_to :dog
 
-    validates :user_id, uniqueness: { scope: :recipe_id }
+    validates :user_id, uniqueness: { scope: [:recipe_id, :dog_id] }
 end
