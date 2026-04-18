96b7e8e23d1b92afec0e1360640e950bb901fabc
Natsumi Hasegawa <chura721@icloud.com>
2026-04-18 10:42:08 +0900

    ユーザー情報編集がめんの追加

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -30,6 +30,21 @@ class UsersController < ApplicationController
     end
   end
 
+  def edit
+    @user = current_user
+  end
+
+  def update
+    @user = current_user
+
+    if @user.update(user_params)
+      redirect_to root_path, notice: "ユーザー情報を更新しました"
+    else
+      render :edit, status: :unprocessable_entity
+    end
+  end
+
+
   private
 
     def user_params
