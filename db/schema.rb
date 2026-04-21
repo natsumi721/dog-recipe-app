264b6a750d931579d89d02a37c51e6dfaf2f82c0
Natsumi Hasegawa <chura721@icloud.com>
2026-04-21 18:35:12 +0900

    Authenticationモデル追加

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.2].define(version: 2026_04_14_020848) do
+ActiveRecord::Schema[7.2].define(version: 2026_04_21_093355) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -40,6 +40,15 @@ ActiveRecord::Schema[7.2].define(version: 2026_04_14_020848) do
     t.bigint "blob_id", null: false
     t.string "variation_digest", null: false
     t.index ["blob_id", "variation_digest"], name: "index_active_storage_variant_records_uniqueness", unique: true
+  end
+
+  create_table "authentications", force: :cascade do |t|
+    t.bigint "user_id", null: false
+    t.string "provider"
+    t.string "uid"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_authentications_on_user_id"
   end
 
   create_table "bookmarks", force: :cascade do |t|
@@ -108,6 +117,7 @@ ActiveRecord::Schema[7.2].define(version: 2026_04_14_020848) do
 
   add_foreign_key "active_storage_attachments", "active_storage_blobs", column: "blob_id"
   add_foreign_key "active_storage_variant_records", "active_storage_blobs", column: "blob_id"
+  add_foreign_key "authentications", "users"
   add_foreign_key "bookmarks", "dogs"
   add_foreign_key "bookmarks", "recipes"
   add_foreign_key "bookmarks", "users"
