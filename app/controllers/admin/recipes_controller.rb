32c7b2cc674a87453482a90b0406debec6e794ec
Natsumi Hasegawa <chura721@icloud.com>
2026-04-23 22:01:00 +0900

    adminの修正

--- app/controllers/admin/recipes_controller.rb
+++ app/controllers/admin/recipes_controller.rb
@@ -1,43 +1,43 @@
-class Admin::RecipesController < ApplicationController
-    before_action :require_login
-    before_action :require_admin
-    before_action :set_recipe, only: [ :show, :update ]
+class Admin::RecipesController < Admin::BaseController  # ← ここを変更
+  before_action :set_recipe, only: [ :show, :update ]
 
-    # 下書きレシピ一覧
-    def index
-      @recipes = Recipe.draft
-    end
+  # 下書きレシピ一覧
+  def index
+    @recipes = Recipe.draft
+  end
 
-    # 承認済みレシピ一覧
-    def published
-      @recipes = Recipe.published.order(updated_at: :desc)
-    end
+  # 承認済みレシピ一覧
+  def published
+    @recipes = Recipe.published.order(updated_at: :desc)
+  end
 
-    def show
-    end
+  def show
+  end
 
-    def update
+  def update
+  # 下書き状態のレシピのみ更新可能にする
+  unless @recipe.draft?
+    redirect_to admin_recipes_path, alert: "このレシピは既に処理されています"
+    return
+  end
+
     case params[:commit]
     when I18n.t("admin.recipes.actions.approve")
-        @recipe.published!
-        redirect_to published_admin_recipes_path, notice: "承認済みに移動しました"
+      @recipe.published!
+      redirect_to published_admin_recipes_path, notice: "承認済みに移動しました"
 
     when I18n.t("admin.recipes.actions.reject")
-        @recipe.rejected!
-        redirect_to admin_recipes_path, notice: "却下しました"
+      @recipe.rejected!
+      redirect_to admin_recipes_path, notice: "却下しました"
 
     else
-        redirect_to admin_recipes_path, alert: "不正な操作です"
+      redirect_to admin_recipes_path, alert: "不正な操作です"
     end
   end
 
-    private
+  private
 
-    def set_recipe
-       @recipe = Recipe.find(params[:id])
-    end
-
-    def require_admin
-      redirect_to root_path unless current_user.admin?
-    end
+  def set_recipe
+    @recipe = Recipe.find(params[:id])
+  end
 end
